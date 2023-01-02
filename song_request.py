6a6745a8236d9d360c694cb54f967867c134378a
algeirn <120324556+algeirn@users.noreply.github.com>
2023-01-02 12:12:41 +0100

    Update song_request.py

--- song_request.py
+++ song_request.py
@@ -1,15 +1,25 @@
+# once pip install pynput done
 import logging
 import webbrowser, threading
+from pynput.keyboard import Key, Controller
 
-artist = str(input('Which artist are you looking for ? '))
-search = str(input('What song of ' + artist + ' are you looking for ? (format word1+word2) '))
+song = str(input("What song are you looking for ? "))
+keyboard = Controller()
+
+def select_searchbar():
+  keyboard.press(Key.tab)
+  keyboard.release(Key.tab)
 
 def run():
-  url = "https://www.youtube.com/results?search_query=" + artist + '+' + song
+  url = "https://www.youtube.com/"
   logging.info("Starting browser at")
   threading.Timer(1.25, lambda: webbrowser.open_new(url)).start()
+  for select_searchbar() in range(4):
+    select_searchbar()  
+  keyboard.press(song)
+  keyboard.press(Key.enter)
   
-def search(artist, song):
+def search(song):
   run()
   
-search(artist, song)
+search(song)
